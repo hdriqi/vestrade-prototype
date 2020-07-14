990bb12afa47f0f07c7160c245d485537ec2aa97
hdriqi <ra@evius.id>
2020-07-14 14:25:30 +0700

    add indexer

--- contracts/Vestrade_ERC20_Factory.sol
+++ contracts/Vestrade_ERC20_Factory.sol
@@ -6,14 +6,14 @@ import "./Vestrade_ERC20.sol";
 contract Vestrade_ERC20_Factory is Ownable {
 	event TokenCreated(string name, address addr);
 	mapping(string => address) tokens;
-	
-	function create(string memory name, string memory symbol, uint8 decimals) onlyOwner public {
+
+	function create(string memory name, string memory symbol, uint8 decimals) public onlyOwner {
 		require(tokens[name] == address(0), "Tokens already exist");
-		Vestrade_ERC20 newToken = new Vestrade_ERC20(name, symbol, decimals);   
+		Vestrade_ERC20 newToken = new Vestrade_ERC20(name, symbol, decimals);
 
 		newToken.addMinter(msg.sender);
 		tokens[name] = address(newToken);
-		
+
 		emit TokenCreated(name, address(newToken));
 	}
 }
